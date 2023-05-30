ec84c1b2a351260723cf2fab5bdecc1c79802406
lenbrocki <45183581+lenbrocki@users.noreply.github.com>
2023-05-30 18:09:27 +0200

    Update sliceviewer.py

--- sliceviewer.py
+++ sliceviewer.py
@@ -4,17 +4,25 @@ import numpy as np
 class sliceviewer:
     def __init__(self, image):
         self.image = image
+        
+        # without plt.ioff() the figure is plotted twice
         plt.ioff()
         self.fig = plt.figure()
         plt.ion()
+
         # init_arr is needed for plt.imshow() to be properly set up for printing of the image slices
         init_arr = np.zeros(image.shape[1:]).astype(image.dtype)
         init_arr[10,10] = image.max()
         init_arr[20,10] = image.min()
         self.im = plt.imshow(init_arr, cmap="gray")
+
         # plot first slice on init
-        self.im.set_data(self.image[0,:,:])
-        self.slider = widgets.IntSlider(value=0, min=0, max=image.shape[0])
+        img_init = self.image[0,:,:]
+        self.im.set_data(img_init)
+        self.im.set_clim(img_init.min(), img_init.max())
+
+        # set up widgets
+        self.slider = widgets.IntSlider(value=0, description="slice", min=0, max=image.shape[0])
         self.view = widgets.Dropdown(
             options=[0,1,2],
             description='axis',
@@ -26,11 +34,13 @@ class sliceviewer:
         self.slider.max = self.image.shape[axis] - 1
 
         if(axis == 0):
-            self.im.set_data(self.image[self.slider.value,:,:])
+            img = self.image[self.slider.value,:,:]
         elif(axis == 1):
-            self.im.set_data(self.image[:,self.slider.value,:])
+            img = self.image[:,self.slider.value,:]
         else:
-            self.im.set_data(self.image[:,:,self.slider.value])
+            img = self.image[:,:,self.slider.value]
+        self.im.set_data(img)
+        self.im.set_clim(img.min(), img.max())
         self.fig.canvas.draw_idle()
     
     def plot(self):
