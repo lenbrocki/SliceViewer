76870a90fa91eb82f4ce66e3bd353e1223b31c5f
lenbrocki <45183581+lenbrocki@users.noreply.github.com>
2023-05-30 16:48:43 +0200

    Update sliceviewer.py

--- sliceviewer.py
+++ sliceviewer.py
@@ -1,33 +1,39 @@
-from ipywidgets import interactive, fixed, widgets, interact
+from ipywidgets import widgets
 import matplotlib.pyplot as plt
-import sys
-
-this = sys.modules[__name__]
-
-x_widget = widgets.IntSlider(min=0, max=100)
-y_widget = widgets.Dropdown(
-    options=[0,1,2],
-    description='axis',
-    value = 0
-)
-
-def update_pos_range(*args):
-    x_widget.max = this.shape[y_widget.value] - 1
+import numpy as np
+class sliceviewer:
+    def __init__(self, image):
+        self.image = image
+        plt.ioff()
+        self.fig = plt.figure()
+        plt.ion()
+        # init_arr is needed for plt.imshow() to be properly set up for printing of the image slices
+        init_arr = np.zeros(image.shape[1:]).astype(image.dtype)
+        init_arr[10,10] = image.max()
+        init_arr[20,10] = image.min()
+        self.im = plt.imshow(init_arr, cmap="gray")
+        # plot first slice on init
+        self.im.set_data(self.image[0,:,:])
+        self.slider = widgets.IntSlider(value=0, min=0, max=image.shape[0])
+        self.view = widgets.Dropdown(
+            options=[0,1,2],
+            description='axis',
+            value = 0
+        )
     
+    def update(self,_):
+        axis = self.view.value
+        self.slider.max = self.image.shape[axis] - 1
 
-y_widget.observe(update_pos_range, 'value')
-
-def plotter(pos, axis, image):
-    if(axis == 0):
-        plt.matshow(image[pos,:,:], cmap="gray")
-    elif(axis == 1):
-        plt.matshow(image[:,pos,:], cmap="gray")
-    else:
-        plt.matshow(image[:,:,pos], cmap="gray")
-    plt.show()
-
-def plot(image):
-    this.shape = image.shape
-    x_widget.max = this.shape[0] - 1
-
-    return interact(plotter, pos=x_widget, axis=y_widget, image=fixed(image))
+        if(axis == 0):
+            self.im.set_data(self.image[self.slider.value,:,:])
+        elif(axis == 1):
+            self.im.set_data(self.image[:,self.slider.value,:])
+        else:
+            self.im.set_data(self.image[:,:,self.slider.value])
+        self.fig.canvas.draw_idle()
+    
+    def plot(self):
+        self.slider.observe(self.update)
+        self.view.observe(self.update)
+        return widgets.VBox(children=[self.slider, self.view, self.fig.canvas])
